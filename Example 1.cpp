7fb467cde8cbdfd896ceeb5617c89e71b4a3be37
Claricel Ramos <claricelramos1904@gmail.com>
2023-09-14 23:59:16 -0400

    added prework

---
+++ Example 1.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main()              /*a simple program*/
+{
+int num;            /*define a variable called num*/
+num = 1;            /*assign a value to num*/
+printf("I am a simple")      /* use the printf() function*/
+printf("computer.\n");
+printf("My favorite number is % because it is first.\n",num);
+system("pause")
+}
---
+++ helloworld.c
@@ -0,0 +1,10 @@
+/* filename: 1B-helloworld.c */
+// same program as 1A but without so many comments
+#include<stdio.h>
+main()
+{
+printf("Hello World\n"); // \n means 'new line'
+// when \n is inside the quotation marks, the program prints a new line
+
+system ("pause");
+} 
